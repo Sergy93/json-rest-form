a5eae4a64848ec2a0400a796847bb9c23e48d340
Sergy93 <takanuva7493@hotmail.com>
2014-10-08 13:56:40 +0200

    First commit

---
+++ ServicioAlumnos/Controllers/alumnosController.cs
@@ -0,0 +1,111 @@
+﻿using ServicioAlumnos.Models;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace ServicioAlumnos.Controllers
+{
+    public class AlumnosController : ApiController
+    {
+        private readonly ajaxEntities _db = new ajaxEntities();
+
+        // GET: api/alumnos
+        public IQueryable<alumnos> Getalumnos()
+        {
+            return _db.alumnos;
+        }
+
+        // GET: api/alumnos/5
+        [ResponseType(typeof(alumnos))]
+        public IHttpActionResult Getalumnos(int id)
+        {
+            alumnos alumnos = _db.alumnos.Find(id);
+            if (alumnos == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(alumnos);
+        }
+
+        // PUT: api/alumnos/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putalumnos(int id, alumnos alumnos)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != alumnos.id)
+            {
+                return BadRequest();
+            }
+
+            _db.Entry(alumnos).State = EntityState.Modified;
+
+            try
+            {
+                _db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AlumnosExists(id))
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/alumnos
+        [ResponseType(typeof(alumnos))]
+        public IHttpActionResult Postalumnos(alumnos alumnos)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _db.alumnos.Add(alumnos);
+            _db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { alumnos.id }, alumnos);
+        }
+
+        // DELETE: api/alumnos/5
+        [ResponseType(typeof(alumnos))]
+        public IHttpActionResult Deletealumnos(int id)
+        {
+            alumnos alumnos = _db.alumnos.Find(id);
+            if (alumnos == null)
+            {
+                return NotFound();
+            }
+
+            _db.alumnos.Remove(alumnos);
+            _db.SaveChanges();
+
+            return Ok(alumnos);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AlumnosExists(int id)
+        {
+            return _db.alumnos.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ ServicioAlumnos/scripts/ajax1.js
@@ -0,0 +1,256 @@
+﻿var url = "http://localhost:50119/api/";
+var urlFinal = url + "alumnos";
+//var urlFinal = 'http://alumnos-mcsd2014.azure-mobile.net/tables/cursos';
+
+
+
+var saltoDeLinea = function () {
+    var br = document.createElement("br");
+    return br;
+}
+
+var borrarTabla = function () {
+    var contenido = document.getElementById("contenido");
+    contenido.removeChild(document.getElementById("tablaDatos"));
+}
+var borrarForm = function () {
+    document.getElementById("formDiv").removeChild(
+        document.getElementById("form"));
+}
+
+var cargarHTML = function (datos) {
+    borrarTabla();
+    borrarForm();
+
+    cargarTabla(datos);
+    cargarForm(datos);
+}
+
+var cargarTabla = function (datos) {
+    var tabla = document.createElement("table");
+
+    var contenido = document.getElementById("contenido");
+    tabla.setAttribute("id", "tablaDatos");
+
+    for (var i = 0; i < datos.length; i++) {
+        var fila = document.createElement("tr");
+
+
+        for (var param in datos[i]) {
+            if (param == "id") fila.setAttribute("id", datos[i][param]);
+
+            var td = document.createElement("td");
+            var text = document.createTextNode(param + " = " + datos[i][param]);
+
+            td.appendChild(text);
+            fila.appendChild(td);
+        }
+        tabla.appendChild(fila);
+    }
+    contenido.appendChild(tabla);
+
+    var filas = document.getElementsByTagName('tr');
+
+    for (var j = 0; j < filas.length; j++) {
+        filas[j].onclick = function (evt) {
+            var seleccionados = document.getElementsByClassName("seleccionado");
+
+            for (var j = 0; j < seleccionados.length; j++) {
+                seleccionados[j].className = "";
+            }
+            evt.target.parentElement.className = "seleccionado";
+        }
+    }
+    //Creamos el botón de borrar
+    var btnBorrar = document.createElement("input");
+    btnBorrar.value = "Borrar";
+
+    btnBorrar.setAttribute("id", "btnUpdate");
+    btnBorrar.setAttribute("type", "button");
+
+    btnBorrar.onclick = borrarElemento;
+
+    tabla.appendChild(btnBorrar);
+
+    //Creamos el botón de actualizar
+    var btnUpdate = document.createElement("input");
+    btnUpdate.value = "Actualizar";
+
+    btnUpdate.setAttribute("id", "btnUpdate");
+    btnUpdate.setAttribute("type", "button");
+
+    btnUpdate.onclick = actualizarElemento;
+
+    tabla.appendChild(btnUpdate);
+}
+
+var cargarForm = function (datos) {
+
+    var form = document.createElement("form");
+    form.setAttribute("id", "form");
+
+    for (var param in datos[0]) {
+
+        if (param != "$id" && param != "id") {
+            var input = document.createElement("input");
+
+            //Creamos los atributos
+            var attrId = document.createAttribute("id");
+            attrId.value = param;
+            var attrPlaceholder = document.createAttribute("placeholder");
+            attrPlaceholder.value = param;
+            var attrType = document.createAttribute("type");
+            attrType.value = "text";
+
+            //Añadimos los atributos
+            input.setAttributeNode(attrId);
+            input.setAttributeNode(attrPlaceholder);
+            input.setAttributeNode(attrType);
+
+            form.appendChild(input);
+            form.appendChild(saltoDeLinea());
+        }
+    }
+
+    //Creamos el botón de guardar
+    var btnGuardar = document.createElement("input");
+    btnGuardar.value = "Guardar";
+
+    attrType = document.createAttribute("type");
+    attrType.value = "button";
+
+    attrId = document.createAttribute("id");
+    attrId.value = "btnGuardar";
+
+    btnGuardar.setAttributeNode(attrId);
+    btnGuardar.setAttributeNode(attrType);
+
+    btnGuardar.onclick = escribirDatos;
+
+    form.appendChild(btnGuardar);
+
+    document.getElementById("formDiv").appendChild(form);
+}
+
+var leerDatos = function () {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", urlFinal, true);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status >= 200 && xhr.status < 300) {
+            var datos = eval(xhr.responseText);
+            cargarHTML(datos);
+        }
+    }
+    xhr.send();
+}
+
+var escribirDatos = function () {
+
+    var inputs = document.getElementsByTagName('input');
+    var nombres = [];
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].getAttribute("type") == "text") {
+            if (!inputs[i].value)
+                return;
+
+            nombres.push([inputs[i].getAttribute("id"), inputs[i].value]);
+        }
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", urlFinal, true);
+
+    xhr.setRequestHeader("Content-type", "application/json");
+
+    var json = {};
+    for (var j = 0; j < nombres.length; j++) {
+        Object.defineProperty(json, nombres[j][0], {
+            configurable: true,
+            enumerable: true,
+            writable: true,
+            value: nombres[j][1]
+        });
+    }
+
+    var jsonText = JSON.stringify(json);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            leerDatos();
+        }
+    }
+    xhr.send(jsonText);
+}
+
+var borrarElemento = function () {
+    var seleccionados = document.getElementsByClassName("seleccionado");
+
+    var id = seleccionados[0].getAttribute("id");
+
+    borrarPorId(id);
+}
+
+var borrarPorId = function (id) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("DELETE", urlFinal + "/" + id, true);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            leerDatos();
+        }
+    }
+    xhr.send();
+}
+var actualizarElemento = function () {
+    var seleccionados = document.getElementsByClassName("seleccionado");
+
+    var id = seleccionados[0].getAttribute("id");
+
+    actualizarPorId(id);
+
+}
+var actualizarPorId = function (id) {
+    var inputs = document.getElementsByTagName('input');
+    var nombres = [];
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].getAttribute("type") == "text") {
+            if (!inputs[i].value)
+                return;
+
+            nombres.push([inputs[i].getAttribute("id"), inputs[i].value]);
+        }
+    }
+
+    var metodo = urlFinal.indexOf("azure") > -1 ? "PATCH" : "PUT";
+
+    var xhr = new XMLHttpRequest();
+    xhr.open(metodo, urlFinal + "/" + id, true);
+
+    xhr.setRequestHeader("Content-type", "application/json");
+
+    var json = {};
+    for (var j = 0; j < nombres.length; j++) {
+        Object.defineProperty(json, nombres[j][0], {
+            configurable: true,
+            enumerable: true,
+            writable: true,
+            value: nombres[j][1]
+        });
+    }
+    json.id = id;
+
+    var jsonText = JSON.stringify(json);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            leerDatos();
+        }
+    }
+    xhr.send(jsonText);
+}
+
+leerDatos();
+
